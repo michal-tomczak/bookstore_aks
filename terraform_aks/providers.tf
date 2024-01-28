2549dc82231c6b4cac5d64b382b5697f8c4f4cb0
root <root@DESKTOP-KD66QER.localdomain>
2024-01-28 21:19:16 +0100

    Added env vars to providers.tf

--- terraform_aks/providers.tf
+++ terraform_aks/providers.tf
@@ -24,8 +24,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id   = ${env.ARM_SUBSCRIPTION_ID}
+  tenant_id         = ${env.ARM_TENANT_ID}
+  client_id         = ${env.ARM_CLIENT_ID}
+  client_secret     = ${env.ARM_CLIENT_SECRET}
 }
