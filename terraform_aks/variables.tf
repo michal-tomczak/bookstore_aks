bc4d8eb18fa54512c6e4d6c361b7ee91431b60bd
root <root@DESKTOP-KD66QER.localdomain>
2024-01-29 13:02:41 +0100

    Changed cluster location to a eu region

--- terraform_aks/variables.tf
+++ terraform_aks/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_location" {
   type        = string
-  default     = "ger-north-east"
+  default     = "germanywestcentral"
   description = "Location of the resource group."
 }
 
