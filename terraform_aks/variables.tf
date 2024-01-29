daae1a8b5c793e16bd13be6c2dbfab409491daef
root <root@DESKTOP-KD66QER.localdomain>
2024-01-29 12:58:51 +0100

    Added vars required for aks creation in providers and variables

--- terraform_aks/variables.tf
+++ terraform_aks/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_location" {
   type        = string
-  default     = "eastus"
+  default     = "ger-north-east"
   description = "Location of the resource group."
 }
 
@@ -13,7 +13,7 @@ variable "resource_group_name_prefix" {
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
-  default     = 3
+  default     = 2
 }
 
 variable "msi_id" {
@@ -26,4 +26,20 @@ variable "username" {
   type        = string
   description = "The admin username for the new cluster."
   default     = "azureadmin"
+}
+
+variable ARM_SUBSCRIPTION_ID {
+  type = string
+}
+
+variable ARM_TENANT_ID {
+  type = string
+}
+
+variable ARM_CLIENT_ID {
+  type = string
+}
+
+variable ARM_CLIENT_SECRET {
+  type = string
 }
