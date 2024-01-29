4edf91a582f286831ee11620cdc3e01e2e559e29
root <root@DESKTOP-KD66QER.localdomain>
2024-01-29 14:40:00 +0100

    Moved extract script inside terraform_aks

--- extract_kubeconfig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$(terraform output kube_config)" > ./azurek8s;
-sed -i '/EOT/d' ./terraform_aks/azurek8s;
-sed -i '/^$/d' ./terraform_aks/azurek8s;
-export KUBECONFIG=./terraform_aks/azurek8s;
-kubectl get nodes
