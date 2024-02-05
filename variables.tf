c10b1201c942a49961ec58790dfa9ace4ad8cf59
root <root@DESKTOP-KD66QER.localdomain>
2024-02-05 13:48:02 +0100

    Added sa with statefile in it, changed rg name to static var

--- variables.tf
+++ variables.tf
@@ -10,6 +10,12 @@ variable "resource_group_name_prefix" {
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
+variable "rg_name" {
+  type = string
+  default ="rg-bookstore"
+  description = "Default name of my rg"
+}
+
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
